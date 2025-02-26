b682eb5199c62afd35bbbb03e8d7fa13c164c83f
eskander-taher <eskander.alshaibany@gmail.com>
2025-02-26 21:04:03 +0300

    add langauges

--- src/models/news.model.ts
+++ src/models/news.model.ts
@@ -3,7 +3,11 @@ import mongoose, { Document, Schema } from "mongoose";
 export interface INews extends Document {
 	id?: string;
 	title: string;
+	titleEnglish?: string;
+	titleRussian?: string;
 	description: string;
+	descriptionEnglish?: string;
+	descriptionRussian?: string;
 	thumbnail?: string;
 	images?: string[];
 	isImportant?: boolean;
@@ -13,7 +17,11 @@ export interface INews extends Document {
 
 const NewsSchema: Schema = new Schema({
 	title: { type: String, required: true },
+	titleEnglish: { type: String },
+	titleRussian: { type: String },
 	description: { type: String, required: true },
+	descriptionEnglish: { type: String },
+	descriptionRussian: { type: String },
 	thumbnail: { type: String },
 	images: { type: [String] },
 	isImportant: { type: Boolean, default: false },
