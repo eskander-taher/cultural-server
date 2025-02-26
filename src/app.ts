c9f8585bae2fd3ef2e163acd0b2e2dc89a51e6d6
eskander-taher <eskander.alshaibany@gmail.com>
2025-02-26 21:24:00 +0300

    add cors

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
 	cors({
-		origin: "https://admin-ruddy-eight.vercel.app",
+		origin: ["https://admin-ruddy-eight.vercel.app", "https://yemculru.com"],
 		methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
 		allowedHeaders: ["Content-Type", "Authorization"],
 	})
